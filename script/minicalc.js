c4b107ba4745223ec5ef9e1e5f6b201e7003fe0a
Lovelyn Tijesunimi-Israel <lovelynisrael@hotmail.com>
2015-08-04 20:03:37 +0100

    Add changes to JS, HTML and CSS

--- script/minicalc.js
+++ script/minicalc.js
@@ -1,41 +1,59 @@
-var resultDisplay = document.getElementById('display');
-var user_input1 = parseInt(document.getElementById('num1').value);
-var user_input2 = parseInt(document.getElementById('num2').value);
+var operatorDisplay = document.getElementById('display');
+var inputDisplay1 = document.getElementById('inputDisplay1');
+var inputDisplay2 = document.getElementById('inputDisplay2');
 
 
 
 function getInput1() {
-    document.getElementById("display");
-    ('display').innerHTML = parseInt(user_input1);
+  console.log(user_input1, 'has been called');
+  var user_input1 = parseInt(document.getElementById('num1').value); 
+  var inputDisplay1 = document.getElementById("inputDisplay1");
+  inputDisplay1.innerHTML = parseInt(user_input1);
 }
 
 function getInput2() {
- var resultDisplay= document.getElementById("display");
-    ('display').innerHTML = parseInt(user_input2);
+  console.log("I'm working fine!");
+  var user_input2 = parseInt(document.getElementById('num2').value);
+  var inputDisplay2= document.getElementById("inputDisplay2");
+  inputDisplay2.innerHTML += parseInt(user_input2);
 }
 
 function updateResultDisplay(operator){
-if(operator == '+'){
-  var resultDisplay = document.getElementById("display");
-    ('display').innerHTML= '+';}
-else if(operator == '-'){
-  var resultDisplay = document.getElementById("display");
-    ('display').innerHTML= '-';}
-else if(operator == '*'){
-  var resultDisplay = document.getElementById("display");
-    ('display').innerHTML= '*';}
-else if(operator == '/'){
-  var resultDisplay = document.getElementById("display");
-    ('display').innerHTML= '/';}
-else if(operator == '%'){
-  var resultDisplay = document.getElementById("display");
-    ('display').innerHTML= '%';}
-else if(operator == '^'){
-  var resultDisplay = document.getElementById("display");
-  ('display').innerHTML= '^';}
-else if(operator == 'C'){
-  var resultDisplay = document.getElementById("display");
-  ('display').innerHTML= ' ';}
+    if(operator == '+'){
+      var  operatorDisplay = document.getElementById("operatorDisplay");
+       operatorDisplay.innerHTML = '+';
+    }
+
+    else if(operator == '-'){
+      var  operatorDisplay = document.getElementById("operatorDisplay");
+       operatorDisplay.innerHTML += '-';
+    }
+
+    else if(operator == '*'){
+      console.log(operator, " has been called!");
+      var  operatorDisplay = document.getElementById("operatorDisplay");
+       operatorDisplay.innerHTML += '*';
+    }
+
+    else if(operator == '/'){
+      var  operatorDisplay = document.getElementById("operatorDisplay");
+      operatorDisplay.innerHTML += '/';
+    }
+
+    else if(operator == '%'){
+      var  operatorDisplay = document.getElementById("operatorDisplay");
+       operatorDisplay.innerHTML += '%';
+    }
+
+    else if(operator == '^'){
+      var  operatorDisplay = document.getElementById("operatorDisplay");
+       operatorDisplay.innerHTML += '^';
+    }
+
+    else if(operator == 'C'){
+      var  operatorDisplay = document.getElementById("operatorDisplay");
+       operatorDisplay.innerHTML= ' ';
+    }
 }
 
 function compute() {
