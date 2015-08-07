b38124aeb72113232e89722c5305a5a1e3fb436c
Lovelyn Tijesunimi-Israel <lovelynisrael@hotmail.com>
2015-08-07 14:29:13 +0100

    Add changes to the compute function

--- script/minicalc.js
+++ script/minicalc.js
@@ -1,7 +1,9 @@
 var operatorDisplay = document.getElementById('display');
 var inputDisplay1 = document.getElementById('inputDisplay1');
 var inputDisplay2 = document.getElementById('inputDisplay2');
-
+var firstInput;
+var secondInput;
+var operator;
 
 
 function getInput1() {
@@ -9,6 +11,7 @@ function getInput1() {
   var inputDisplay1 = document.getElementById("inputDisplay1");
   inputDisplay1.innerHTML = user_input1;
   console.log(user_input1, 'has been called');
+  firstInput = inputDisplay1.innerHTML;
 }
 
 function getInput2() {
@@ -16,6 +19,7 @@ function getInput2() {
   var inputDisplay2= document.getElementById("inputDisplay2");
   inputDisplay2.innerHTML += user_input2;
   console.log(user_input2, 'has been called');
+  secondInput = inputDisplay2.innerHTML;
 }
 
 function updateResultDisplay(operator){
@@ -63,6 +67,7 @@ function updateResultDisplay(operator){
       var inputDisplay2 = document.getElementById("inputDisplay2");
       inputDisplay2.innerHTML = ' ';
     }
+    operator = operatorDisplay.innerHTML;
 }
 
 
@@ -78,30 +83,34 @@ function updateResultDisplay(operator){
 
 
 function compute(){
-  var user_input1 = parseInt(document.getElementById('num1').value);
-  var user_input2 = parseInt(document.getElementById('num2').value);
-  var  operatorDisplay = document.getElementById("operatorDisplay");
-  console.log(user_input1);
-  console.log(operatorDisplay.innerHTML);
-  console.log(user_input2);
-  if(operatorDisplay.innerHTML == '+'){
-    return user_input1+ user_input2
-  }
-    else if (operatorDisplay.innerHTML == '-'){
-      return user_input1- user_input2
-    }
-    else if (operatorDisplay.innerHTML == '*'){
-      return user_input1* user_input2
-    }
-    else if (operatorDisplay.innerHTML == '/'){
-      return user_input1/ user_input2
-    }
-    else if (operatorDisplay.innerHTML == '%'){
-      return user_input1% user_input2
-    }
-    else if (operatorDisplay.innerHTML == '^'){
-      return user_input1^ user_input2
-    }
+  var finalResultDisplay = document.getElementById('finalResultDisplay');
+  finalResultDisplay.innerHTML = eval(firstInput, operator, secondInput);
+  console.log('The result is ...', finalResultDisplay.innerHTML);
+  console.log(firstInput);
+  console.log(secondInput);
+  console.log(operator);
+  // var  operatorDisplay = document.getElementById("operatorDisplay");
+  // console.log(user_input1);
+  // console.log(operatorDisplay.innerHTML);
+  // console.log(user_input2);
+  // if(operatorDisplay.innerHTML == '+'){
+  //   return user_input1+ user_input2
+  // }
+  //   else if (operatorDisplay.innerHTML == '-'){
+  //     return user_input1- user_input2
+  //   }
+  //   else if (operatorDisplay.innerHTML == '*'){
+  //     return user_input1* user_input2
+  //   }
+  //   else if (operatorDisplay.innerHTML == '/'){
+  //     return user_input1/ user_input2
+  //   }
+  //   else if (operatorDisplay.innerHTML == '%'){
+  //     return user_input1% user_input2
+  //   }
+  //   else if (operatorDisplay.innerHTML == '^'){
+  //     return user_input1^ user_input2
+  //   }
 //   var  resultDisplay = document.getElementById("resultDisplay");
 //   console.log(resultDisplay.value);
 //   var FinalResultDisplay = document.getElementById("finalResultDisplay");
